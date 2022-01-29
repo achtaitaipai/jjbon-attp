7f22153f90ce5d53194e04f397e2eaa457956902
Charles Cailleteau <charles.cailleteau@protonmail.com>
2022-01-29 21:27:22 +0100

    dialog

--- main.lua
+++ main.lua
@@ -1,19 +1,24 @@
+require("modules/utils")
 require("modules/loadTiledMap")
 require("modules/player")
 require("modules/spriteSheet")
 require("modules/anim")
 require("modules/camera")
+require("modules/dialogBox")
 
 function love.load()
     --scale pixels without blur
     love.graphics.setDefaultFilter("nearest", "nearest")
+
+    local font = love.graphics.newImageFont( "assets/abcmin.png", "'abcdefghijklmnopqrstuvwxyz[!]~ " )
+    love.graphics.setFont(font)
 
     --create spriteSheet
     -- path to the file, width of each sprite, height of each sprite
     local spriteSheet=loadSpriteSheet("assets/attpjjbon.png",8,8)
 
     --define the anims of the player
-    local playerSpriteSheet=loadSpriteSheet('assets/playerAnimx16.png',16,16)
+    local playerSpriteSheet=loadSpriteSheet('assets/playerAnim.png',8,8)
     local fps =8
     playerAnims={
         --spriteSheet, index of the frames to display in the order, fps, flip the image or not, frame to display in peuse mode
@@ -28,16 +33,31 @@ function love.load()
     _G.map=loadTiledMap(mapFile, spriteSheet)
     --create the player
     --position x in the grid, position y in the gris, animations array with: top, bottom, left and right
-    _G.player=createPlayer(7,7,playerAnims,"bottom",16,16)
+    _G.player=createPlayer(7,7,playerAnims,"bottom",8,8)
     _G.camera=createCamera(32,32,2)
+    _G.dialogBox=createDialogBox(128,16,16)
 end
 
 function love.update(dt)
-    _G.player:update(dt)
+    if _G.dialogBox.active==false then
+        _G.player:update(dt)
+    end
+    _G.dialogBox:up()
 end
 
 function love.draw()
+
+    love.graphics.push()
     _G.camera:up()
     _G.map:drawTiles()
     _G.player:draw()
+    love.graphics.pop()
+    
+    _G.dialogBox:draw()
 end
+
+function love.keypressed( key )
+    if key == "space" then
+       _G.dialogBox:next()
+    end
+ end
---
+++ modules/dialogBox.lua
@@ -0,0 +1,78 @@
+function createDialogBox(width,height,margin)
+    local dialogBox = {
+        width=width,
+        height=height,
+        margin=margin,
+        active=true,
+        xR=(love.graphics.getWidth()-(width+margin))*0.5,
+        yR=love.graphics.getHeight()-(height+margin*3),
+        xT=(love.graphics.getWidth()-(width))*0.5,
+        yT=love.graphics.getHeight()-(height+margin*2),
+        txt="bonjour comment ca*va !*je suis ravi de*l'apprendre",
+        index=1,
+        keyPressed=false,
+        delay=0.05,
+        time=0,
+        charIndex=1,
+        txtShow=""
+    }
+    local txtArr=string.explode(dialogBox.txt,'*')
+    local txt={}
+    for i=0, math.round(#txtArr/2)-1 do
+        local txt1=txtArr[i*2+1] or ""
+        local txt2=txtArr[i*2+2] or ""
+        table.insert(txt,txt1..'\n'..txt2)
+    end
+    dialogBox.txt=txt
+
+    function dialogBox:up()
+
+        if love.keyboard.isDown('space') and self.keyPressed == false then
+            self.keyPressed=true
+        else
+            self.keyPressed=false
+        end
+    end
+
+    function dialogBox:draw()
+        if self.active then
+            self:animText()
+            love.graphics.rectangle(
+                "line",
+                self.xR,
+                self.yR,
+                self.width+self.margin*2,
+                self.height+self.margin*2
+            )
+            love.graphics.print(self.txtShow,self.xT,self.yT)
+        end
+
+    end
+
+    function dialogBox:animText()
+        local txt =self.txt[self.index]
+        local t = love.timer.getTime()
+
+        if self.time<t and self.charIndex <= #txt then
+            self.time=t+self.delay
+            self.txtShow=txt:sub(0,self.charIndex)
+            self.charIndex=self.charIndex+1
+        end
+
+    end
+
+    function dialogBox:next()
+        local txt =self.txt[self.index]
+        if self.charIndex >= #txt then
+            self.index=self.index+1
+            self.time=0
+            self.charIndex=1
+            self.txtShow=""
+            if self.index > #self.txt then
+                self.index=1
+                self.active=false
+            end
+        end
+    end
+    return dialogBox
+end
